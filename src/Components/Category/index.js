6ba7c3aa0724947e02ce07c9b78517f60da49ec7
Vinicius Komninakis <komninakis@porquenao.mobi>
2019-06-09 19:00:57 -0300

    feat: add envet to resize

--- src/Components/Category/index.js
+++ src/Components/Category/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {css} from 'glamor';
 import Colors from '../../Colors';
 
-export default function index(props) {
+export default function Category(props) {
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
     const wrapper = css({
         width: '100%',
         height: '100%',
@@ -14,6 +18,7 @@ export default function index(props) {
         '&::-webkit-scrollbar': {
             'display': 'none',
           },
+         justifyContent: width > 570? 'center' : null,
     });
     const title = css({
         fontFamily: "Montserrat,sans-serif",
@@ -23,7 +28,21 @@ export default function index(props) {
         lineHeight: "13px",
         color: Colors.greyBold,
         margin:'32px 0 17px 15px',
+        textAlign: width > 570? 'center' : null,
       });
+
+      function resize(){
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+      }
+
+      useEffect(()=>{
+          window.addEventListener('resize',resize);
+          return function remove(){
+              window.removeEventListener('resize',resize);
+          }
+      })
+
     return (
         <section {...wrapper}>
             <h2 {...title}>Categorias</h2>
